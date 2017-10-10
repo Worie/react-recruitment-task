4b799dc87a30cac4f463fc0164a18f0c736572ae
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2017-10-11 01:34:21 +0200

    sticky menu

--- app/containers/Menu.js
+++ app/containers/Menu.js
@@ -2,32 +2,37 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { switchTab } from '../actions';
-import { menu } from '../styles/menu.scss';
+import {
+  menu,
+  menuWrapper
+} from '../styles/menu.scss';
 
 const Menu = ({ onShowContent }) => {
   let li;
   return (
     <nav className={menu}>
-      <h2>
-        Menu
-      </h2>
-      <ul>
-        <li>
-          <a href="#showPosts" onClick={() => onShowContent('posts')}>
-            Show posts
-          </a>
-        </li>
-        <li>
-          <a href="#showComments" onClick={() => onShowContent('comments')}>
-            Comments
-          </a>
-        </li>
-        <li>
-          <a href="#showAlbums" onClick={() => onShowContent('albums')}>
-            Albums
-          </a>
-        </li>
-      </ul>
+      <div className={menuWrapper}>
+        <h2>
+          Menu
+        </h2>
+        <ul>
+          <li>
+            <a href="#showPosts" onClick={() => onShowContent('posts')}>
+              Show posts
+            </a>
+          </li>
+          <li>
+            <a href="#showComments" onClick={() => onShowContent('comments')}>
+              Comments
+            </a>
+          </li>
+          <li>
+            <a href="#showAlbums" onClick={() => onShowContent('albums')}>
+              Albums
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
